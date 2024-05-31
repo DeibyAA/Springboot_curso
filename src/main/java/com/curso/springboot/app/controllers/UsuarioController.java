e7d837f9c51d959aa557483d096bfd2dd00a6a33
DeibyAA <deibyarroyave4@gmail.com>
2024-05-31 17:00:46 -0500

    tabla usuarios

--- src/main/java/com/curso/springboot/app/controllers/UsuarioController.java
+++ src/main/java/com/curso/springboot/app/controllers/UsuarioController.java
@@ -4,6 +4,8 @@ import com.curso.springboot.app.models.Usuario;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -19,6 +21,44 @@ public class UsuarioController {
         usuario.setTelefono("3011580102");
 
         return usuario;
+    }
+
+    @RequestMapping(value = "usuarios")
+    public List<Usuario> getUsuario() {
+        List<Usuario> usuarios = new ArrayList<>();
+        Usuario usuario = new Usuario();
+        usuario.setId(1L);
+        usuario.setNombre("Deiby");
+        usuario.setApellido("Arroyave");
+        usuario.setEmail("deibhyarroyave@gmail.com");
+        usuario.setTelefono("3011580102");
+
+        Usuario usuario2 = new Usuario();
+        usuario2.setId(12L);
+        usuario2.setNombre("Gínes");
+        usuario2.setApellido("Miñano");
+        usuario2.setEmail("qwertyui@gmail.com");
+        usuario2.setTelefono("1234567");
+
+        Usuario usuario3 = new Usuario();
+        usuario3.setId(123L);
+        usuario3.setNombre("Nacho");
+        usuario3.setApellido("Argentino");
+        usuario3.setEmail("deiboiuykfgmail.com");
+        usuario3.setTelefono("312344502");
+
+        Usuario usuario4 = new Usuario();
+        usuario4.setId(1234L);
+        usuario4.setNombre("Mike");
+        usuario4.setApellido("Shinobi");
+        usuario4.setEmail("dei@il.com");
+        usuario4.setTelefono("300000002");
+
+        usuarios.add(usuario);
+        usuarios.add(usuario2);
+        usuarios.add(usuario3);
+        usuarios.add(usuario4);
+        return usuarios;
     }
 
     @RequestMapping(value = "usuario123")
