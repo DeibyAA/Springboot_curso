f718cee9b324c605d3a6e4aeec894815c124317b
DeibyAA <deibyarroyave4@gmail.com>
2024-06-22 08:26:54 -0500

    avances

--- src/main/java/com/curso/springboot/app/dao/usuarioDaoImp.java
+++ src/main/java/com/curso/springboot/app/dao/usuarioDaoImp.java
@@ -1,16 +1,22 @@
 package com.curso.springboot.app.dao;
 
 import com.curso.springboot.app.models.Usuario;
+import jakarta.persistence.PersistenceContext;
 import org.springframework.stereotype.Repository;
-//import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.annotation.Transactional;
 
+import jakarta.persistence.EntityManager;
 import java.util.List;
 
 @Repository
-//@Transactional
+@Transactional
 public class usuarioDaoImp implements usuarioDao {
+
+    @PersistenceContext
+    EntityManager entityManager;
+
     @Override
     public List<Usuario> getUsuario() {
-        return List.of();
+        String query = "FROM Usuario";
     }
 }
