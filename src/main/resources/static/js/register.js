49aaf8270ac6b872f9e27ce01b5fa435d0a4b804
DeibyAA <deibyarroyave4@gmail.com>
2024-07-16 20:01:45 -0500

    solucion de problemas

--- src/main/resources/static/js/register.js
+++ src/main/resources/static/js/register.js
@@ -4,13 +4,16 @@ $(document).ready(function() {
 });
 
 async function registrarUsuario() {
-      const request = await fetch('api/usuarios', {
-        method: 'POST',
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        }
-      });
-      const usuarios = await request.json();
+  let datos = {};
+
+  const request = await fetch('api/usuarios', {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    }
+    body: JSON.stringify({a: 1, b: 'Textual content'})
+  });
+  const usuarios = await request.json();
 }
 
